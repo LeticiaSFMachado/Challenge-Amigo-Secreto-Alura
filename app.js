f57e82001df1c3d97168fa8f15b530e6fcc6042c
Letícia Machado <126581029+LeticiaSFMachado@users.noreply.github.com>
2025-08-01 12:22:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let amigos = [];
+
+function adicionarAmigo() {
+   const input = document.querySelector('input');
+   const nome = input.value.trim();
+    
+    if (nome === "") {
+        alert("Insira um nome.");
+    } else {
+        alert("Amigo inserido.");
+    }
+
+    amigos.push(nome);
+    console.log(amigos);
+
+    atualizarLista();
+
+    input.value = '';
+    input.focus();
+
+}
+
+function atualizarLista( ){
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        const item = document.createElement("li");     // Cria <li>
+            item.textContent = amigos[i];              // Define o texto
+            lista.appendChild(item);                   // Adiciona ao <ul>
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        let numeroSorteado = Math.floor(Math.random() * amigos.length)
+        let nomeSorteado = amigos[numeroSorteado];
+        console.log(nomeSorteado);
+        
+        const resultado = document.getElementById("resultado");
+        resultado.innerHTML = ''; // limpa antes de exibir novo
+
+        const item = document.createElement("li");
+        item.textContent = `Amigo sorteado: ${nomeSorteado}`;
+        resultado.appendChild(item);
+
+    }
+    else {
+        alert ("A lista está vazia.")
+    }
+}
+    
+
+
